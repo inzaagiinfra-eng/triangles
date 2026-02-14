d10d4ea1abf3ae27bf5252721002046eb2d19817
Inzaagi infra <inzaagiinfra@gmail.com>
2026-02-14 18:16:06 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='h-screen lg:text-4xl max-lg:text-2xl text-red-500'>
+        😊Coming Soon...
+      </div>
+    </>
+  )
+}
+
+export default App
